08a2b48c0d7f026aa546d56e491cb4a16889c5f5
Lee-SeungMin <51289006+Seungminnni@users.noreply.github.com>
2025-09-08 13:42:30 +0900

    한글 유니코드 관련 출력 숫자형에 대입 가능하지만 하나의 유니코드를 하나의 바이트 안에 넣을 수 없다

--- Practice_01/Datatype03.java
+++ Practice_01/Datatype03.java
@@ -8,7 +8,16 @@ public class Datatype03 {
         char c4 = 'A';
         char c5 = '1';
         char c6 = '.';
-
+        int x = 0;
+        for (char i = '가'; i <= '힣'; i++) {
+            x++;
+            System.out.println(i);
+            if(x%20 == 0) {
+                {
+                    System.out.println();
+                }
+            }
+        }
         System.out.println(c1);
         System.out.println(c2);
         System.out.println(c3);
