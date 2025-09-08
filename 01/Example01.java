ca51be11f2f3db1e70fdd17a34d06444e5c6419e
Lee-SeungMin <51289006+Seungminnni@users.noreply.github.com>
2025-09-08 12:59:51 +0900

    변수 선언과 상수선언 실습해보기 1. Hello 클래스 선언, 날짜 선언 속도 선언해서 출력하기 2. Example01 클래스 선언하고 SPEED를 상수로 선언 교수님 강의 : 객체 자료형은 String 같은 경우, String객체를 만들어 선언해서 str.toUpper()와 같은 식으로 호출이 가능함 변수명의 대문자 소문자 시작에 따라 다르다 설명하심

---
+++ 01/Example01.java
@@ -0,0 +1,16 @@
+
+public class Example01 {
+	public static void main(String[] args)
+	{
+		final int SPEED;
+		int count = 10;
+		SPEED = 40;
+		count = 11;
+
+		System.out.println("상수 SPEED 값은");
+		System.out.println(SPEED);
+		System.out.println("변수 count 값은");
+		System.out.println(count);
+	}
+
+}
---
+++ 01/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+	public static void main(String[] args)
+	{
+		int days = 10;
+		int speed;
+		speed = 20;
+		
+		System.out.println(days);
+		System.out.println(speed);
+	}
+
+}
