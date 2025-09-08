eaeee07c425aa28b214aa51e78796728f58bbd68
Lee-SeungMin <51289006+Seungminnni@users.noreply.github.com>
2025-09-08 13:52:16 +0900

    문자형 자료형과 숙제 관련 파일 업로드

---
+++ Homework_01/hw1.java
@@ -0,0 +1,18 @@
+package Homework_01;
+
+public class hw1 {
+    public static void main(String[] args) {
+        String name = "seungmin";
+        int age = 25;
+        float height = 175.5f;
+        char grade = 'S';
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+
+}
---
+++ Practice_01/Datatype04.java
@@ -0,0 +1,13 @@
+package Practice_01;
+
+public class Datatype04 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Java";
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str1 + str2);
+    }
+    
+}
