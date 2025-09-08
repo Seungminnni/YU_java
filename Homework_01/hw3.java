95b04b6a43db418d2688e7b78ed3ab0d20ad86b0
Lee-SeungMin <51289006+Seungminnni@users.noreply.github.com>
2025-09-08 14:12:03 +0900

    출력처리와 숙제 관련 코드 업로드

---
+++ Homework_01/hw3.java
@@ -0,0 +1,13 @@
+package Homework_01;
+
+import java.util.Scanner;
+
+public class hw3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("섭씨 온도를 입력하시오: ");
+        double celsius = scanner.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.printf("%.1f\n", fahrenheit);
+    }
+}
