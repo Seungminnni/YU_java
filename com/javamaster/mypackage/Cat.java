eab45eaab9c8e2669225b41e96f1a1deb0b52441
Seungminnni <lsm020505@gmail.com>
2025-11-03 15:01:59 +0900

    패키지관련 개발

---
+++ com/javamaster/mypackage/Cat.java
@@ -0,0 +1,19 @@
+package com.javamaster.mypackage;
+
+public class Cat {
+    String breed;
+    String color;
+
+    public void eat() {
+        System.out.println("The cat is eating.");
+    }
+
+    public void scratch() {
+        System.out.println("The cat is scratching.");
+    }
+
+    public void meow() {
+        System.out.println("Meow!");
+    }
+    
+}
---
+++ com/section02/Package01.java
@@ -0,0 +1,11 @@
+package com.section02;
+
+import com.javamaster.mypackage.Cat;
+public class Package01 {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.eat();
+        myCat.scratch();
+        myCat.meow();
+    }
+}
